5a1e32778caa2cfd7abecbe25a1afb1ac7e6f791
padoling <33655848+padoling@users.noreply.github.com>
2020-01-25 16:08:24 +0900

    MainPageLoad 테스트 완료

---
+++ src/main/java/com/padoling/portfolio/august/web/IndexController.java
@@ -0,0 +1,13 @@
+package com.padoling.portfolio.august.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/index")
+    public String index() {
+        return "index";
+    }
+}
